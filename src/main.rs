9c3a8c260c75c892b08e084337d43ef35c21b6a3
gjf2a <ferrer@hendrix.edu>
2021-01-27 08:19:27 -0600

    Replaced unwrap() calls with ?

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::env::args;
 use std::fs::File;
 use std::io::{BufRead,BufReader};
+use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
     for arg in args().skip(1) {
-        let input_file = File::open(arg).unwrap();
+        let input_file = File::open(arg)?;
         let reader = BufReader::new(input_file);
         for line in reader.lines() {
-            println!("{}", line.unwrap());
+            println!("{}", line?);
         }
     }
+    Ok(())
 }
